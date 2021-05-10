c2f6e87cf7a3bdebfaa1c605733f0462e2145cb0
Kevin Huynh <kevinhuynh269@yahoo.com>
2021-05-10 15:48:42 -0500

    Number Manipulator

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NumberManipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var result = Int32.Parse(Console.ReadLine());
+           result *= result;
+           result+=14;
+           result-=3;
+           Console.WriteLine(result);
+        }
+    }
+}
